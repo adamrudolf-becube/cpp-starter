2c12997efc128f5b7fb599806c10556323586b01
Rudolf Ádám <rudolf.adam.a@gmail.com>
2023-11-01 11:19:58 +0100

    Add glog for logging

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <glog/logging.h>
 
-int main()
+int main(int argc, char* argv[])
 {
+    google::InitGoogleLogging(argv[0]);
     std::cout << "Hello world!" << std::endl;
+    LOG(INFO) << "Example log message";
 }
